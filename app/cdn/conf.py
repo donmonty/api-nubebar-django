6aa65c651602e11f7db55929530f05fe4729581f
Montero <antonio.montero@gmail.com>
2021-09-20 21:02:43 -0500

    Fix S3 settings

--- app/cdn/conf.py
+++ app/cdn/conf.py
@@ -1,5 +1,5 @@
 import os
-
+"""
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME="nubebar"
@@ -11,3 +11,18 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 DEFAULT_FILE_STORAGE="app.cdn.backends.MediaRootS3Boto3Storage"
 STATICFILES_STORAGE="app.cdn.backends.StaticRootS3Boto3Storage"
+"""
+
+
+
+AWS_ACCESS_KEY_ID = 'ZJDHSVYL4ZD4Y3MGTMML'
+AWS_SECRET_ACCESS_KEY = 'C7DQQGL2R5tkD1XBbToaSK2fo9ati4eGtzUFpapDQhc'
+AWS_STORAGE_BUCKET_NAME = 'nubebar-assets'
+AWS_S3_ENDPOINT_URL = 'https://sfo3.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'nubebar-static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- app/settings.py
+++ app/settings.py
@@ -191,8 +191,9 @@ USE_TZ = True
 # https://nubebar.sfo3.digitaloceanspaces.com
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 
@@ -201,6 +202,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # in production, we want cdn
 #MEDIA_ROOT = BASE_DIR / "staticfiles-cdn" / "uploads"
 #from .cdn.conf import * # noqa
+
+AWS_ACCESS_KEY_ID = 'ZJDHSVYL4ZD4Y3MGTMML'
+AWS_SECRET_ACCESS_KEY = 'C7DQQGL2R5tkD1XBbToaSK2fo9ati4eGtzUFpapDQhc'
+AWS_STORAGE_BUCKET_NAME = 'nubebar-assets'
+AWS_S3_ENDPOINT_URL = 'https://sfo3.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'nubebar-static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 AUTH_USER_MODEL = 'core.User'
 
